6a364760321d4321747dc8b4309838f68f86a2ae
Alexander Grenkov <a.grenkov@gmail.com>
2024-10-28 05:00:22 +0500

    add: pages app

---
+++ pages/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import HomePage
+
+@admin.register(HomePage)
+class HomePageAdmin(admin.ModelAdmin):
+    list_display = ("name", "title")
+
+    def has_add_permission(self, request):
+        return not HomePage.objects.exists()
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
