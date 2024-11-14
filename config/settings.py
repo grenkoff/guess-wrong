519d4685f08e96e2a479fb11dfaf208b3cd45a79
Alexander Grenkov <a.grenkov@gmail.com>
2024-11-15 02:50:06 +0500

    fix: settings.py

--- config/settings.py
+++ config/settings.py
@@ -8,12 +8,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_OAUTH_CLIENT_ID = os.environ['GOOGLE_OAUTH_CLIENT_ID']
-if not GOOGLE_OAUTH_CLIENT_ID:
-    raise ValueError(
-        'GOOGLE_OAUTH_CLIENT_ID is missing.' 
-        'Have you put it in a file at config/.env ?'
-    )
+# GOOGLE_OAUTH_CLIENT_ID = os.environ['GOOGLE_OAUTH_CLIENT_ID']
+# if not GOOGLE_OAUTH_CLIENT_ID:
+#     raise ValueError(
+#         'GOOGLE_OAUTH_CLIENT_ID is missing.' 
+#         'Have you put it in a file at config/.env ?'
+#     )
 
 # We need these lines below to allow the Google sign in popup to work.
 SECURE_REFERRER_POLICY = 'no-referrer-when-downgrade'
