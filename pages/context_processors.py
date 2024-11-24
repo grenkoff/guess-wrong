47e7529122234bd77b82bec3aaa7bc1ec9e2d974
Alexander Grenkov <a.grenkov@gmail.com>
2024-11-24 08:29:17 +0500

    fix: rewrite canonical_url

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -9,9 +9,11 @@ def home_view(request):
 
 def canonical_url(request: HttpRequest):
     """
-    Контекстный процессор для генерации канонического URL
+    Контекстный процессор для генерации канонического URL с HTTPS
     """
     canonical_path = request.build_absolute_uri(request.path)
+    # Принудительно заменить http:// на https://
+    canonical_path = canonical_path.replace("http://", "https://", 1)
     return {
         'CANONICAL_URL': canonical_path
     }
